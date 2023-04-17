3f5253310c0daa677bcb96ccf0a4aef4451eea49
Adeputras17 <kirabelkparg@gmail.com>
2023-04-17 18:00:22 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Todolist from "./components/Todo"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Todolist />
+    </div>
+  );
+}
+
+export default App;
